c03a10555ac8ca556597a247cfb24c033e8ddc4f
jeff4elee <jel347@ucsd.edu>
2017-01-14 17:39:18 -0800

    v1.11
    Fixed popup refresh issue

--- Chrome Extension/background.js
+++ Chrome Extension/background.js
@@ -3,3 +3,4 @@ chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if (request.type == "status") sendResponse({status: localStorage.status});
 });
+
--- Chrome Extension/popup.js
+++ Chrome Extension/popup.js
@@ -1,11 +1,9 @@
-if(!localStorage.status) localStorage.status = "On";
-
 //toggles the state of status in localstorage
 //communicates to content.js with background.js as proxy
 function toggle(){
  
   var state = document.getElementById("toggle").innerText;
-
+  console.log(state);
   if(state == "Turn On"){
 
     document.getElementById("toggle").innerHTML = "Turn Off";
@@ -27,10 +25,33 @@ function toggle(){
     throw "Unexpected toggle error";
 
   }
+
 }
 
 //waits until the page is fully loaded, then awaits click to toggle
 document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById("toggle").addEventListener("click", toggle)
+
+	if(!localStorage.status) localStorage.status = "On";
+
+	//popup.html resets per window open/close
+	//save state in localStore, then load the proper
+	//attributes and image for the browserAction and popup
+
+	var state = localStorage.status;
+	var buttonState;
+
+	if(state == "On"){
+		buttonState = "Off";
+		chrome.browserAction.setIcon({path: "active38.png"});
+	} else{
+		buttonState = "On";
+		chrome.browserAction.setIcon({path: "inactive38.png"});
+	}
+	
+    document.getElementById("toggle").innerHTML = "Turn " + buttonState;
+
+    //wait for user click to toggle the button
+	document.getElementById("toggle").addEventListener("click", toggle)
+
 });
 
